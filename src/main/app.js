bd8d4d5f76fa81b3f0e509d5424ec62360749a34
Masaya Nasu <tomato.wonder.life@gmail.com>
2016-10-02 02:45:16 +0900

    follow eslint comment

--- src/main/app.js
+++ src/main/app.js
@@ -1,23 +1,24 @@
-'use strict';
+'use strict'
 
-var electron = require('electron');
-var app = electron.app;
-var BrowserWindow = electron.BrowserWindow;
+import electron from 'electron'
 
-var mainWindow = null;
+var app = electron.app
+var BrowserWindow = electron.BrowserWindow
+
+var mainWindow = null
 
 app.on('window-all-closed', function() {
   if (process.platform != 'darwin')
-    app.quit();
-});
+    app.quit()
+})
 
 app.on('ready', function() {
 
   // ブラウザ(Chromium)の起動, 初期画面のロード
-  mainWindow = new BrowserWindow({width: 800, height: 600});
-  mainWindow.loadURL('file://' + __dirname + '/../index.html');
+  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow.loadURL('file://' + __dirname + '/../index.html')
 
   mainWindow.on('closed', function() {
-    mainWindow = null;
-  });
-});
+    mainWindow = null
+  })
+})
