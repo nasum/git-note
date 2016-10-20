5e4b231a784840f859cc55a31fd505676279c8f1
Masaya Nasu <tomato.wonder.life@gmail.com>
2016-10-21 08:29:19 +0900

    add menu sample

--- src/main/app.js
+++ src/main/app.js
@@ -1,10 +1,12 @@
 import electron from 'electron'
+import * as menu from './menu'
 
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 const dialog = electron.dialog
+const {Menu} = require('electron')
 
-const dir_path
+let dir_path = ""
 
 let mainWindow = null
 
@@ -20,6 +22,103 @@ app.on('ready', () => {
   mainWindow.on('closed', () => {
     mainWindow = null
   })
-
+  initMenu(Menu)
   dir_path = dialog.showOpenDialog(mainWindow, {title: 'プロジェクトを開いてください',properties: ['openDirectory', 'createDirectory']})
 })
+
+function initMenu(Menu){
+  const template = [
+    {
+      label: 'Edit',
+      submenu: [
+        {
+          role: 'undo'
+        },
+        {
+          role: 'redo'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          role: 'cut'
+        },
+        {
+          role: 'copy'
+        },
+        {
+          role: 'paste'
+        },
+        {
+          role: 'pasteandmatchstyle'
+        },
+        {
+          role: 'delete'
+        },
+        {
+          role: 'selectall'
+        }
+      ]
+    },
+    {
+      label: 'View',
+      submenu: [
+        {
+          label: 'Reload',
+          accelerator: 'CmdOrCtrl+R',
+          click (item, focusedWindow) {
+            if (focusedWindow) focusedWindow.reload()
+          }
+        },
+        {
+          label: 'Toggle Developer Tools',
+          accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
+          click (item, focusedWindow) {
+            if (focusedWindow) focusedWindow.webContents.toggleDevTools()
+          }
+        },
+        {
+          type: 'separator'
+        },
+        {
+          role: 'resetzoom'
+        },
+        {
+          role: 'zoomin'
+        },
+        {
+          role: 'zoomout'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          role: 'togglefullscreen'
+        }
+      ]
+    },
+    {
+      role: 'window',
+      submenu: [
+        {
+          role: 'minimize'
+        },
+        {
+          role: 'close'
+        }
+      ]
+    },
+    {
+      role: 'help',
+      submenu: [
+        {
+          label: 'Learn More',
+          click () { require('electron').shell.openExternal('http://electron.atom.io') }
+        }
+      ]
+    }
+  ]
+
+  const myMenu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(myMenu)
+}
